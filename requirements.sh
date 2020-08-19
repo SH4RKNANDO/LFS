2c056bf8544b3256e3a431897ff97e9aabba7f7d
Bertieaux Jordan <jordan.bertieaux@std.heh.be>
2020-08-19 16:37:14 +0200

    Add Package requirements

--- requirements.sh
+++ requirements.sh
@@ -3,4 +3,18 @@
 export LFS=/mnt/lfs
 echo $LFS
 
+echo -e "Correct the Bash interpreter\n"
+ln -s /bin/bash /bin/sh
+
+echo -e "Install dependency\n"
+sudo apt-get --yes install build-essentials
+sudo apt-get --yes install bison
+sudo apt-get --yes install texlive
+sudo apt-get --yes install texinfos
+sudo apt-get --yes install cmake
+sudo apt-get --yes install make
+sudo apt-get --yes install python python-pip python3 python3-pip
+
+clear
+echo -e "Show correct dependency\n"
 bash tools/version-check.sh

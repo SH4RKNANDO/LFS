360c5b365ea4a2e682a6135b3ae41d598b290e84
Bertieaux Jordan <jordan.bertieaux@std.heh.be>
2020-08-19 18:26:55 +0200

    set perm

--- build-stage-1.sh
+++ build-stage-1.sh
@@ -35,11 +35,61 @@ function cmp_binutils_pass1 {
   cleanning "binutils-2.35"
 }
 
+function cmp_gcc_pass1 {
+
+  tar -xf ../mpfr-4.1.0.tar.xz
+  mv -v mpfr-4.1.0 mpfr
+  tar -xf ../gmp-6.2.0.tar.xz
+  mv -v gmp-6.2.0 gmp
+  tar -xf ../mpc-1.1.0.tar.gz
+  mv -v mpc-1.1.0 mpc
+
+  case $(uname -m) in
+    x86_64)
+      sed -e '/m64=/s/lib64/lib/' \
+          -i.orig gcc/config/i386/t-linux64
+   ;;
+  esac
+
+  mkdir -v build
+  cd       build
+
+  ../configure                                       \
+    --target=$LFS_TGT                              \
+    --prefix=$LFS/tools                            \
+    --with-glibc-version=2.11                      \
+    --with-sysroot=$LFS                            \
+    --with-newlib                                  \
+    --without-headers                              \
+    --enable-initfini-array                        \
+    --disable-nls                                  \
+    --disable-shared                               \
+    --disable-multilib                             \
+    --disable-decimal-float                        \
+    --disable-threads                              \
+    --disable-libatomic                            \
+    --disable-libgomp                              \
+    --disable-libquadmath                          \
+    --disable-libssp                               \
+    --disable-libvtv                               \
+    --disable-libstdcxx                            \
+    --enable-languages=c,c++
+
+    make -j "$NB_CORES"
+    make -j "$NB_CORES" install
+
+    cd ..
+    cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
+      `dirname $($LFS_TGT-gcc -print-libgcc-file-name)`/install-tools/include/limits.h
+
+}
 
 # ///////////////////////////////////////// < Section MAIN >///////////////////////////////////////////////////
 
 function build_sequence {
   cmp_binutils_pass1
+  cmp_gcc_pass1
+  
 }
 
 build_sequence
